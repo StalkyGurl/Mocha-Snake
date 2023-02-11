d0c87b1c9a8935ceed9d47c64be0bc0be55e02f1
AMSek2003 <am.sek@wp.pl>
2023-02-11 22:31:48 +0100

    Added score, restart game + some graphic changes

--- main.py
+++ main.py
@@ -10,27 +10,77 @@ from coffee import *
 SNAKE_SPEED = 6
 FPS = 60
 WIDTH = HEIGHT = 700
+SCORE_SPACE = 50
 DIMENSION = 20
 SQ_SIZE = WIDTH // DIMENSION
-screen = p.display.set_mode((WIDTH, HEIGHT))
+screen = p.display.set_mode((WIDTH, HEIGHT + SCORE_SPACE))
 p.display.set_caption('Mocha Snake')
 clock = p.time.Clock()
 bg = p.image.load("images/game_bg.png")
+bg = p.transform.scale(bg, (WIDTH, HEIGHT))
+BEST_SCORE = 0
+
+
+def get_font(size):
+    return p.font.Font("assets/font.ttf", size)
+
+
+def printScore(score):
+    p.font.init()
+    font = get_font(55)
+    text = font.render(f'Score: ', True, '#1AB1E5')
+    screen.blit(text, (10,690))
+    font2 = p.font.SysFont('Verdana', 46)
+    font2.set_bold(True)
+    text2 = font2.render(f'{score}', True, '#1AB1E5')
+    screen.blit(text2, (225, 695))
+
+
+def printEndGameText(score):
+    global BEST_SCORE
+    p.font.init()
+    font = get_font(55)
+    text1 = font.render('Game Over!', True, '#1AB1E5')
+    shadow1 = font.render('Game Over!', True, '#0D5660')
+    text2 = font.render(f'Score: {score}', True, '#1AB1E5')
+    shadow2 = font.render(f'Score: {score}', True, '#0D5660')
+    text3 = font.render(f'Best Score: {BEST_SCORE}', True, '#1AB1E5')
+    shadow3 = font.render(f'Best Score: {BEST_SCORE}', True, '#0D5660')
+    text4 = font.render('New best score!', True, '#1AB1E5')
+    shadow4 = font.render('New best score!', True, '#0D5660')
+
+    font2 = get_font(30)
+    shadow5 = font2.render("Press 'r' to restart the game", True, '#0D5660')
+    text5 = font2.render("Press 'r' to restart the game", True, '#1693BE')
+    screen.blit(shadow5, (WIDTH/2 - text5.get_width()/2 + 2, HEIGHT//2 + 355 + 2))
+    screen.blit(text5, (WIDTH/2 - text5.get_width()/2, HEIGHT//2 + 355))
+
+    screen.blit(shadow1, (WIDTH/2 - text1.get_width()/2 + 2, HEIGHT//2 - 100 + 2))
+    screen.blit(shadow2, (WIDTH/2 - text2.get_width()/2 + 2, HEIGHT//2 - 50 + 2))
+    screen.blit(text1, (WIDTH/2 - text1.get_width()/2, HEIGHT//2 - 100))
+    screen.blit(text2, (WIDTH/2 - text2.get_width()/2, HEIGHT//2 - 50))
+    if score != BEST_SCORE:
+        screen.blit(shadow3, (WIDTH/2 - text3.get_width()/2 + 2, HEIGHT//2 + 2))
+        screen.blit(text3, (WIDTH/2 - text3.get_width()/2, HEIGHT//2))
+    else:
+        screen.blit(shadow4, (WIDTH/2 - text4.get_width()/2 + 2, HEIGHT//2 + 2))
+        screen.blit(text4, (WIDTH/2 - text4.get_width()/2, HEIGHT//2))
 
 
 def main():
     p.init()
-    screen.fill('black')
     snake = Snake()
     running = True
     coffee = spawnCoffee(snake.coordinates)
     eaten = False
+    end_game = False
     while running:
+        screen.fill('black')
         screen.blit(bg, (0, 0))
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-            elif e.type == p.KEYDOWN:
+            elif e.type == p.KEYDOWN and not end_game:
                 if e.key == p.K_w and snake.direction != 'down':
                     snake.direction = 'up'
                 elif e.key ==p.K_s and snake.direction != 'up':
@@ -39,26 +89,41 @@ def main():
                     snake.direction = 'left'
                 elif e.key ==p.K_d and snake.direction != 'left':
                     snake.direction = 'right'
+        if not end_game:
+            if snake.coordinates[0] == (coffee.x, coffee.y):
+                eaten = True
+                snake.length += 1
+                del coffee
+                coffee = spawnCoffee(snake.coordinates)
 
-        if snake.coordinates[0] == (coffee.x, coffee.y):
-            eaten = True
-            snake.length += 1
-            del coffee
-            coffee = spawnCoffee(snake.coordinates)
+            printScore(snake.length)
+            coffee.drawCoffee(screen)
+            snake.moveSnake(eaten)
+            snake.drawSnake(screen)
+            eaten = False
+            p.display.flip()
+            sleep(SNAKE_SPEED/100)
+            clock.tick(60)
 
         if snake.endGame():
-            sleep(5)
-            running = False
-
-
-
-        coffee.drawCoffee(screen)
-        snake.moveSnake(eaten)
-        snake.drawSnake(screen)
-        eaten = False
-        p.display.flip()
-        sleep(SNAKE_SPEED/100)
-        clock.tick(60)
+            global BEST_SCORE
+            end_game = True
+            score = snake.length
+            if score > BEST_SCORE:
+                BEST_SCORE = score
+            printEndGameText(score)
+            p.display.flip()
+            clock.tick(60)
+            for e in p.event.get():
+                if e.type == p.QUIT:
+                    running = False
+                elif e.type == p.KEYDOWN and e.key == p.K_r:
+                    del snake
+                    del coffee
+                    snake = Snake()
+                    coffee = spawnCoffee(snake.coordinates)
+                    end_game = False
+                    
 
 
 if __name__  == '__main__':
--- snake.py
+++ snake.py
@@ -6,8 +6,6 @@ import pygame as p
 
 SQ_SIZE = 35
 
-snake_box = p.image.load("images/snake_box.png")
-snake_box = p.transform.scale(snake_box,(SQ_SIZE, SQ_SIZE))
 
 class Snake():
     def __init__(self):
@@ -17,8 +15,37 @@ class Snake():
 
 
     def drawSnake(self,screen):
+        snake_box = p.image.load("images/snake_box.png")
+        snake_head = p.image.load("images/snake_head.png")
+        snake_tail = p.image.load("images/snake_tail.png")
+        snake_box = p.transform.scale(snake_box,(SQ_SIZE, SQ_SIZE))
+        snake_head = p.transform.scale(snake_head,(SQ_SIZE, SQ_SIZE))
+        snake_tail = p.transform.scale(snake_tail,(SQ_SIZE, SQ_SIZE))
         for i in range(self.length):
-            screen.blit(snake_box, self.coordinates[i])
+            if i == 0 and self.length > 1:
+                if self.direction == 'down':
+                    snake_head = p.transform.rotate(snake_head, 0)
+                elif self.direction == 'right':
+                    snake_head = p.transform.rotate(snake_head, 90)
+                elif self.direction == 'up':
+                    snake_head = p.transform.rotate(snake_head, 180)
+                elif self.direction == 'left':
+                    snake_head = p.transform.rotate(snake_head, 270)
+                screen.blit(snake_head, self.coordinates[i])
+            elif self.length > 1 and i == self.length - 1:
+                lx, ly = self.coordinates[i-1]
+                x, y = self.coordinates[i]
+                if lx > x:
+                    snake_tail = p.transform.rotate(snake_tail, 90)
+                elif lx < x:
+                    snake_tail = p.transform.rotate(snake_tail, 270)
+                elif ly > y:
+                    snake_tail = p.transform.rotate(snake_tail, 0)
+                else:
+                    snake_tail = p.transform.rotate(snake_tail, 180)
+                screen.blit(snake_tail, self.coordinates[i])
+            else:
+                screen.blit(snake_box, self.coordinates[i])
 
     
     def moveSnake(self, eaten = False):
